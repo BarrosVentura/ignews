8729c91db3f71dfadc42d0ed733f70d5a73825ba
Adson Ventura <adsonventura@gmail.com>
2022-05-25 18:01:45 -0300

    feat: add home content and style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,7 +9,24 @@ export default function Home() {
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1>Hello World</h1>;
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <Image
+          width={336}
+          height={521}
+          src="/images/avatar.svg"
+          alt="Girl coding"
+        />
+      </main>
     </>
   );
 }
