0a1864d242c47bed4adf63259114f55050b8efec
Adson Ventura <adsonventura@gmail.com>
2022-05-25 16:16:46 -0300

    feat: add title to index page, removes from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,7 @@
 import { AppProps } from "next/app";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>ig.news</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
