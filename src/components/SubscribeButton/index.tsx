31be5e37bc0345ee8e77e635bae0ed4e3d92976d
Adson Ventura <adsonventura@gmail.com>
2022-05-26 17:48:23 -0300

    feat: add axios, stripe-js libs, create subscribe api route for creating a checkout process and a function to handle the user action to subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,6 +8,26 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { status } = useSession();
+
+  async function handleSubscribe() {
+    if (status === "unauthenticated") {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
