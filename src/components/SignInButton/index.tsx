889a77af7061c663e347bb5a2228bd067c49f06a
Adson Ventura <adsonventura@gmail.com>
2022-05-26 14:14:44 -0300

    feat: add next auth package, api route to auth and signIn and Out functions for github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,28 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data, status } = useSession();
 
-  return isUserLoggedIn ? (
+  return status === "authenticated" ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Adson Ventura
-      <FiX color="#737380" className={styles.closeIcon} />
+      {data.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
