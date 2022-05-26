990bc0eb798c26b930f97848371ae4910b54ee1c
Adson Ventura <adsonventura@gmail.com>
2022-05-26 09:31:02 -0300

    feat: add stripe connection and utilize server side props for api call

---
+++ src/services/stripe.ts
@@ -0,0 +1,10 @@
+import Stripe from "stripe";
+import packageJson from "../../package.json";
+
+export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+  apiVersion: "2020-08-27",
+  appInfo: {
+    name: "Ignews",
+    version: packageJson.version,
+  },
+});
