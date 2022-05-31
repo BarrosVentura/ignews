5d2bb5a74a7216e900921eca6f256c837723e635
Adson Ventura <adsonventura@gmail.com>
2022-05-31 08:59:06 -0300

    feat: add new preview posts page

---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,88 @@
+import { asHTML } from "@prismicio/helpers";
+import { GetStaticProps } from "next";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { createClient } from "../../../services/prismic";
+
+import styles from "./post.module.scss";
+
+interface PreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
+}
+
+export default function Preview({ post }: PreviewProps) {
+  const { data } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (data?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [data]);
+
+  return (
+    <>
+      <Head>
+        <title>{post.title} | ig.news</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">
+              <a href="">Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = createClient();
+
+  const response = await prismic.getByUID("post-id", String(slug));
+
+  const post = {
+    slug: response.uid,
+    title: response.data.title,
+    content: asHTML(response.data.content.splice(0, 3)),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+
+  return {
+    props: { post },
+  };
+};
